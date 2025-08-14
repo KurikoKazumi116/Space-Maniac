6ac56e16ff14c88699f9c29575be8bcc4ab0e701
KurikoKazumi116 <gabrielledavaadorjgd@gmail.com>
2025-08-13 20:20:12 -0700

    Made a new plan to start working on the starting page where user is clicking to chat

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-import pygame
+import pygame,time
 from sys import exit
 
 pygame.init()
+pygame.font.init()
+pygame.mixer.init()
 screen = pygame.display.set_mode((800,600))
 
 pygame.display.set_caption('SPACE-MANIA')
-
+fonts = pygame.font.Font('assets/Pixeltype.ttf',50)
 #screens
 Starter_menu = True
 beginCut = False
@@ -14,64 +16,48 @@ gameLVL2 = False
 game_ending = False
 game_over = False
 
+reply = pygame.image.load('assets/')
+reply_rect = reply.get_rect(center = (400,300))
+
+FPS = 60
+level = 1
+lives = 5
 
 player = pygame.image.load('assets/player/0.png')
 player_rect = player.get_rect(center = (20,20))
 clock = pygame.time.Clock()
 
-class ship(): # make a class so multiple images can have the attributes
-    def __init__(self,x,y,color,health = 100): # = sets a base parameter value
-        self.x = x
-        self.y = y
-        self.health = health
-        self.color = color
-        self.shipimg = None
-        self.laserimg = None
-        self.lasers = []
-        self.cooldown = 0
-    def draw(self):
-        pygame.draw.rect(screen, (90,90,0), (self.x, self.y,50,50),0)
+CutText = [
+    "Evening"
+    "Haha. I guess so."
+    "..."
+    "..."
+    "Earth has been taken over."
+    "All COMMS will be disconnected."
 
-def main():
-    run = True
-    FPS = 60
-    level = 1
-    lives = 5
+]
 
-    def game():
-        lives_label = fonts.render(f'Lives: {lives}', 1, (255,225,255))
-        level_label = fonts.render(f'Level: {level}',1,(255,255,255))
-        screen.blit(level_label,(10,10))
-        screen.blit(lives_label,(100,100))
-    while True:
-        ship.draw(screen)
-        game()
+def gameinfo():
+    lives_label = fonts.render(f'Lives: {lives}', 1, (255,225,255))
+    level_label = fonts.render(f'Level: {level}',1,(255,255,255))
+    screen.blit(level_label,(10,10))
+    screen.blit(lives_label,(100,100))
+while True:
+    
+    gameinfo()
 
-
-        
-        fonts = pygame.font.Font('assets/Pixeltype.ttf',50)
-        screen.blit(player, (400,400))
-        
-        
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                exit()
-            if event.type == pygame.KEYDOWN:
-                if event.key ==pygame.K_LEFT:
-                    ship.x +=2
-                    
-                if event.key ==pygame.K_RIGHT:
-                    ship.x += 3
-            if event.type == pygame.KEYUP:
-                if event.key ==pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                    player.left += 20
-        if Starter_menu:
-            screen.fill(0)
-
-
+    
+    screen.blit(player, (400,400))
+    
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if reply_rect.collidepoint == (event.pos)
             
-        pygame.display.update()
-        clock.tick(FPS)
-                
+    pygame.display.update()
+    clock.tick(FPS)
+            
 
